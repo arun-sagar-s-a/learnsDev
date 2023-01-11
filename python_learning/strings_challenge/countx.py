0750498c87ca6a5d3c58db53255b27e1626c3c80
Arun Sagar S A <51088722+arun-sagar-s-a@users.noreply.github.com>
2023-01-11 11:48:59 +0000

    added 2 more functions.

---
+++ python_learning/strings_challenge/countx.py
@@ -0,0 +1,6 @@
+def count_char_x(word,letter):
+  return word.count(letter)
+# Uncomment these function calls to test your tip function:
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
