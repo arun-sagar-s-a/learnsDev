6610c8e8e47160a75126add0c606cea73feda2d3
arun-sagar-s-a <arunsagarsa99@gmail.com>
2023-01-16 16:53:31 +0530

    added greetings message.

--- open-ended-project/portfolio1/StoryTeller.py
+++ open-ended-project/portfolio1/StoryTeller.py
@@ -1,4 +1,9 @@
 """_summary_: Simple portfolio project to understand python better.
     Its a story teller, based on input options you select the story changes.
     Author: Arun Sagar S A 
+    Date:16/Jan/2023
 """
+print('A very warm welcome to you! It is lovely to have you among us!')
+listener_name = input('Can we know your good name: ')
+print(
+    f'We are glad to welcome you {listener_name}!!\nYour presence is well appreciated, and we are delighted you are here.\nPlease feel at home.')
