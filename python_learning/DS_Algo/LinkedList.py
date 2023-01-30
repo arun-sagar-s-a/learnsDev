0ed06be64c37d2f04f1e24c0130d1bce034ed792
arun-sagar-s-a <arunsagarsa99@gmail.com>
2023-01-30 17:36:21 +0530

    added function to remove arbitary node.

--- python_learning/DS_Algo/LinkedList.py
+++ python_learning/DS_Algo/LinkedList.py
@@ -5,7 +5,7 @@ class Node:
         self.next_node = next_node
 
     # get node value
-    def get_vlaue(self):
+    def get_value(self):
         return self.value
     # get next node
 
@@ -35,11 +35,40 @@ class LinkedList:
 
     # display Linked List
     def display(self):
-        temp_node = self.get_head_node()
-        while (temp_node.value != None):
-            print(temp_node.get_vlaue())
-            temp_node = temp_node.get_next_node
+        temp_node = self.get_head_node()  # node
+        # print(temp_node.get_value())
+        while (temp_node != None):
+            print(temp_node.get_value())
+            temp_node = temp_node.get_next_node()
+
+    def remove_node(self, value_to_remove):
+        current_node = self.get_head_node()
+        temp_node = current_node.get_next_node()
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+            print(f"Removed : {value_to_remove}")
+            self.display()
+        else:
+            while temp_node != None:
+                if (temp_node.get_value() != value_to_remove):
+                    current_node = current_node.get_next_node()
+                    temp_node = temp_node.get_next_node()
+                    continue
+                else:
+                    current_node.set_next_node(temp_node.get_next_node())
+                    temp_node.set_next_node(None)
+                    print(f"Removed : {value_to_remove}")
+                    break
+            else:
+                print(f"{value_to_remove} is not present in linked list")
+            self.display()
 
 
 linked_list = LinkedList(10)
+linked_list.add_node_beginning(12)
+linked_list.add_node_beginning(13)
+print("Display function")
 linked_list.display()
+
+print("Remove function")
+linked_list.remove_node(12)
