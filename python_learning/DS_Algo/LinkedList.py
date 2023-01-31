896ce37f02208f428ad00851ba9284a3d68cb886
arun-sagar-s-a <arunsagarsa99@gmail.com>
2023-01-31 10:59:27 +0530

    added swap nodes function

--- python_learning/DS_Algo/LinkedList.py
+++ python_learning/DS_Algo/LinkedList.py
@@ -41,6 +41,7 @@ class LinkedList:
             print(temp_node.get_value())
             temp_node = temp_node.get_next_node()
 
+    # remove any node in Linked list
     def remove_node(self, value_to_remove):
         current_node = self.get_head_node()
         temp_node = current_node.get_next_node()
@@ -63,12 +64,65 @@ class LinkedList:
                 print(f"{value_to_remove} is not present in linked list")
             self.display()
 
+    # swap two nodes in Linked list
+    def swap_nodes(self, val1, val2):
+        # initialise defaults
+        node1 = self.get_head_node()
+        node2 = self.get_head_node()
+        node1_prev = None
+        node2_prev = None
+        # if node1 and node2 are same no swap needed.
+        if val1 == val2:
+            print("No swapping necessary.")
+            return
+        # find val1 node
+        while node1 is not None:
+            if node1.get_value() == val1:
+                break
+            else:
+                node1_prev = node1
+                node1 = node1.get_next_node()
+        else:
+            print(f"{val1} is not present in LL so can't swap.")
+            return
+        # find val2 node
+        while node2 is not None:
+            if node2.get_value() == val2:
+                break
+            else:
+                node2_prev = node2
+                node2 = node2.get_next_node()
+        else:
+            print(f"{val2} is not present in LL so can't swap.")
+            return
+
+        # if prev node is non then current is head
+        if node1_prev is None:
+            self.head_node = node2
+        else:
+            node1_prev.set_next_node(node2)
+        # if prev node is non then current is head
+        if node2_prev is None:
+            self.head_node = node1
+        else:
+            node2_prev.set_next_node(node1)
+
+        # finally swap the values
+        temp_node = node1.get_next_node()
+        node1.set_next_node(node2.get_next_node())
+        node2.set_next_node(temp_node)
+
 
 linked_list = LinkedList(10)
-linked_list.add_node_beginning(12)
-linked_list.add_node_beginning(13)
+for i in range(9):
+    linked_list.add_node_beginning(i)
+
+
+print("Display function")
+# linked_list.display()
+# print("After swapp")
+# print("Remove function")
+# linked_list.remove_node(12)
+linked_list.swap_nodes(1, 9)
 print("Display function")
 linked_list.display()
-
-print("Remove function")
-linked_list.remove_node(12)
